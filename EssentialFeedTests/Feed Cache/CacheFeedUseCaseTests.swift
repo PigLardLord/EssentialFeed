6b6f300b47f5999245fd3bb022e892a0c013590b
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-11-05 18:09:23 +0100

    Save comand invoke cache deletion.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,13 +6,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]){
+        store.deleteCachedFeed()
     }
 }
 
@@ -21,8 +33,26 @@ final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         
-        let _ = LocalFeedLoader(store: store)
+        _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsDeletionOnCall() {
+        let store = FeedStore()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        let sut = LocalFeedLoader(store: store)
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: nil, location: nil, image: anyUrl)
+    }
+    
+    private var anyUrl: URL {
+        return URL(string: "http://a_url.com")!
     }
 }
