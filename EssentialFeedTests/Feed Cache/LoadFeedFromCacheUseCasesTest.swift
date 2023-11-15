0624ff455116b5b06d8be665bc86d5fbf7ad9beb
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-11-15 19:32:22 +0100

    LocalFeedLoader does not clear the cache upon creation (before Loading the cache)

---
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCasesTest.swift
@@ -0,0 +1,65 @@
+//
+//  LoadFeedFromCacheUseCasesTest.swift
+//  EssentialFeedTests
+//
+//  Created by Giovanni Trovato on 15/11/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class LoadFeedFromCacheUseCasesTest: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,  file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let loader = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        return (loader, store)
+    }
+    
+    private class FeedStoreSpy: FeedStore {
+        enum ReceivedMessage: Equatable {
+            case insert([LocalFeedImage], Date)
+            case deleteCachedFeed
+        }
+        
+        private(set) var receivedMessages = [ReceivedMessage]()
+        private var deletionCompletions = [DeletionCompletion]()
+        private var insertionCompletions = [InsertionCompletion]()
+        
+        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+            deletionCompletions.append(completion)
+            receivedMessages.append(.deleteCachedFeed)
+        }
+        
+        func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+            insertionCompletions.append(completion)
+            receivedMessages.append(.insert(items, timestamp))
+        }
+        
+        func completeDeletionWith(error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func completeInsertionWith(error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+    }
+}
