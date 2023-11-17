c32318eb987f0069a880c7859d20619c3f481d0c
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-11-17 20:44:39 +0100

    Command load delivers cached imege on non expired chache.

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCasesTest.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCasesTest.swift
@@ -41,6 +41,17 @@ final class LoadFeedFromCacheUseCasesTest: XCTestCase {
         }
     }
     
+    func test_load_deliversChachedImagesOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let currentDate = Date()
+        let lessThanSevenDaysOldTymestamp = currentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { currentDate } )
+        
+        expect(sut, toCompleteWith: .success(feed.model)) {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTymestamp)
+        }
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -74,5 +85,29 @@ final class LoadFeedFromCacheUseCasesTest: XCTestCase {
     
     private var anyNSError: NSError{
         return NSError(domain: "Any Error", code: 0)
+    }
+    
+    private func uniqueImageFeed() -> (model: [FeedImage], local: [LocalFeedImage]) {
+        let items = [uniqueImage(), uniqueImage()]
+        let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (items, localItems)
+    }
+    
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: nil, location: nil, url: anyUrl)
+    }
+    
+    private var anyUrl: URL {
+        return URL(string: "http://a_url.com")!
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
