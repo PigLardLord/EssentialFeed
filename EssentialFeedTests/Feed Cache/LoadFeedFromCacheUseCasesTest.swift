db317885b75402ed9e60486bb606570fe2c765a7
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-11-18 19:49:29 +0100

    Load command delivers no images on more than seven days old cache.

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCasesTest.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCasesTest.swift
@@ -62,6 +62,18 @@ final class LoadFeedFromCacheUseCasesTest: XCTestCase {
             store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
         }
     }
+    
+    func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let currentDate = Date()
+        let moreThansevenDaysOldTimestamp = currentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { currentDate } )
+        
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrieval(with: feed.local, timestamp: moreThansevenDaysOldTimestamp)
+        }
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
