438171258d7d68afb4c0c9ad5377d6e7dc76e329
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-12-03 19:12:28 +0100

    Ensure the operations that leave side effect on system (insert and delete) runs serlially

--- EssentialFeedTests/Feed Cache/CodableEssentialFeedTests.swift
+++ EssentialFeedTests/Feed Cache/CodableEssentialFeedTests.swift
@@ -125,6 +125,33 @@ final class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(error, "Expected deletion fails with an error")
     }
     
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+
+        let operation1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(operation1)
+            operation1.fulfill()
+        }
+        
+        let operation2 = expectation(description: "Operation 2")
+        sut.deleteCachedFeed { _ in
+            completedOperationsInOrder.append(operation2)
+            operation2.fulfill()
+        }
+        
+        let operation3 = expectation(description: "Operation 3")
+        sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(operation3)
+            operation3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationsInOrder, [operation1, operation2, operation3], "Expected side-effects to run serially but completed in the wrong order")
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(storeUrl: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
