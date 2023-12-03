5c0b53bc37bab781af0681c98a268438b2acf945
Giovanni Trovato <giovanni.trovato@cortado.com>
2023-12-03 19:36:46 +0100

    Add comment to the HTTPClient get method warniong the user that it can run in any thread.

--- EssentialFeed/FeedApi/HttpClient.swift
+++ EssentialFeed/FeedApi/HttpClient.swift
@@ -10,5 +10,7 @@ import Foundation
 public typealias HttpClientResult = Result<(HTTPURLResponse, Data), Error>
 
 public protocol HttpClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate thread if needed.
     func get(from url: URL, completion: @escaping (HttpClientResult) -> Void)
 }
