a7348a3706e5a05514597958959b3a71f611452b
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-02-13 19:19:42 +0100

    Add EssentialFeedCacheIntegrationTests target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,11 @@
+//
+// Created on 13/02/24 using Swift 5.0
+// Copyright © 2024 Cortado AG. All rights reserved.
+//
+        
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
