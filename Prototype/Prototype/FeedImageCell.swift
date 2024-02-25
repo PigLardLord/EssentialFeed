996f4da44a77447c21e1f7c751a3228fa7fbcba4
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-02-25 19:23:07 +0100

    Simulate loading animation on the images

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -2,7 +2,7 @@
 // Created on 25/02/24 using Swift 5.0
 // Copyright © 2024 Cortado AG. All rights reserved.
 //
-        
+
 
 import UIKit
 
@@ -11,4 +11,28 @@ final class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) var locationLabel: UILabel!
     @IBOutlet private(set) var feedImageView: UIImageView!
     @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        feedImageView.alpha = 0
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        feedImageView.alpha = 0
+    }
+    
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
+        
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.3,
+            options: [],
+            animations: {
+                self.feedImageView.alpha = 1
+            })
+    }
 }
