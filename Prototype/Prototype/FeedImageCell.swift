c638a4604687bb335f6925049d25e28055856be8
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-02-25 19:20:18 +0100

    Render demo content into cells.

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+// Created on 25/02/24 using Swift 5.0
+// Copyright © 2024 Cortado AG. All rights reserved.
+//
+        
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+}
