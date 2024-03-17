92ea3f8c7965630b8577bfe6f0bfdc6cafcc07ac
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-03-17 12:44:22 +0100

    Extract Adapter pattern into a separated static function to clarify intent.

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -13,12 +13,15 @@ public final class FeedUIComposer {
     public static func feedComposedWith(loader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: loader)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = {[weak feedController] feed in
-            feedController?.tableModel = feed.map({ model in
-                FeedImageCellController(model: model, imageLoader: imageLoader)
-            })
-            feedController?.tableView.reloadData()
-        }
+        refreshController.onRefresh = adaptFeedToCellControllers(forwordingTo: feedController, loader: imageLoader)
         return feedController
+    }
+    
+    private static func adaptFeedToCellControllers(forwordingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak controller] feed in
+            controller?.tableModel = feed.map({ model in
+                FeedImageCellController(model: model, imageLoader: loader)
+            })
+        }
     }
 }
