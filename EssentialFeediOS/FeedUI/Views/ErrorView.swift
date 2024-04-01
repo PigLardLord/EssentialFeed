986a439c278bd0a8feb57c006b1c3b1ca0384c14
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-04-01 19:02:32 +0200

    Move ErrorView to a separate file

---
+++ EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,11 @@
+//
+// Created on 01/04/24 using Swift 5.0
+// Copyright © 2024 Cortado AG. All rights reserved.
+//
+        
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
