3592c0e84397da583221d96818ad503fa4bbdc40
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-04-01 18:30:12 +0200

    Move error view configuration to storyboard

--- EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/FeedUI/Helpers/FeedViewController+TestHelpers.swift
@@ -57,7 +57,7 @@ extension FeedViewController {
     }
     
     var errorMessage: String? {
-        return errorView.message
+        return errorView?.message
     }
     
     var isShowingLoadingIndicator: Bool {
