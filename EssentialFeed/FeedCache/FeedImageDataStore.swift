293309afd21cdedea8ebb100d097783c650bc663
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-05-03 12:50:41 +0200

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ EssentialFeed/FeedCache/FeedImageDataStore.swift
@@ -0,0 +1,12 @@
+//
+// Created on 03/05/24 using Swift 5.0
+//
+        
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
