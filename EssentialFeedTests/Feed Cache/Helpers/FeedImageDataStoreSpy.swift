ced03c78dab161656ed18db2f3625ba28ba1e193
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-05-04 19:01:23 +0200

    Rename generic `completions` to `retrievalCompletions` to better clarify intent

--- EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -12,7 +12,7 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
         case retrieve(dataFor: URL)
     }
     
-    private var completions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
     private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
 
     private(set) var receivedMessages = [Message]()
@@ -24,15 +24,15 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
     
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         receivedMessages.append(.retrieve(dataFor: url))
-        completions.append(completion)
+        retrievalCompletions.append(completion)
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
-        completions[index](.failure(error))
+        retrievalCompletions[index](.failure(error))
     }
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
-        completions[index](.success(data))
+        retrievalCompletions[index](.success(data))
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
