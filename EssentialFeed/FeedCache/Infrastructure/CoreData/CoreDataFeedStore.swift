a5552f0a96ec0e8aaf92ca289d31f36c714d7284
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-05-05 18:42:11 +0200

    Centralize CoreData bundle creation.

--- EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -11,7 +11,8 @@ public final class CoreDataFeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
-    public init(storeUrl: URL, bundle: Bundle = .main) throws {
+    public init(storeUrl: URL) throws {
+        let bundle = Bundle(for: CoreDataFeedStore.self)
         container = try NSPersistentContainer.load(modelName: "FeedStore", from: storeUrl, in: bundle)
         context = container.newBackgroundContext()
     }
