32432e3dbca5452f3e92b19fc55ed599c935e62e
Giovanni Trovato <giovanni.trovato@cortado.com>
2024-05-08 12:48:14 +0200

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,11 @@
+//
+// Created on 08/05/24 using Swift 5.0
+// Copyright © 2024 Cortado AG. All rights reserved.
+//
+        
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+    
+}
